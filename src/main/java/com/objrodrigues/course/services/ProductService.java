14f0855d4e848b908b73ad544a29181ef624a3c2
Diego Rodrigues <objrodrigues@gmail.com>
2023-11-30 15:12:01 -0300

    Product

---
+++ src/main/java/com/objrodrigues/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.objrodrigues.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.objrodrigues.course.entities.Product;
+import com.objrodrigues.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
